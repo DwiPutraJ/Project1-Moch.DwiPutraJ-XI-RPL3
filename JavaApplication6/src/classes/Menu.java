25a5e644d588c29b78f02bd02d6f16ecd5d210b2
DwiPutraJ <dwiputrajulian24@exmpale.com>
2021-06-02 15:43:00 +0700

    Implementasi Kelas Menu

--- JavaApplication6/src/classes/Menu.java
+++ JavaApplication6/src/classes/Menu.java
@@ -10,5 +10,33 @@ package classes;
  * @author Acer
  */
 public class Menu {
+    private String nama_menu;
+    private double harga;
+    private String kategori;
+    
+    
+    public String getNama_menu() {
+        return nama_menu;
+    }
+    
+    public void setNama_menu(String nama_menu) {
+        this.nama_menu = nama_menu;
+    }
+    
+    public double getharga() {
+        return harga;
+    }
+    
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    public String getKategori() {
+        return kategori;
+    }
+    
+    public void setKategoi (String kategori) {
+        this.kategori = kategori;
+    }
     
 }
